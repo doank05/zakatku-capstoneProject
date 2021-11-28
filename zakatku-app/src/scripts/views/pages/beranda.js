93841ec9461e12cdfae5e53c0b7e1741347704b7
DeniAlpiana <denialpiana12@gmail.com>
2021-11-28 09:39:27 +0700

    Update Tampilan Beranda (1)
    Penambahan konten

--- zakatku-app/src/scripts/views/pages/beranda.js
+++ zakatku-app/src/scripts/views/pages/beranda.js
@@ -1,7 +1,7 @@
 const Beranda = {
   async render() {
     return `
-      <section id="sheet-one" class="sheet-one">
+      <section id="fitur-description" class="fitur-description">
         <div class="jumbotron">
           <img src="jumbotron1-crop.jpg" alt="image-jumbotron">
         </div>
@@ -20,6 +20,29 @@ const Beranda = {
             <img src="catatan-icon.png" alt="icon-catatan">
             <h2>CATATAN</h2>
             <p>Simpan riwayat perhitungan zakatmu agar dapat dikelola dengan baik.</p>
+          </div>
+        </div>
+        <div class="application-description">
+          <p>Zakatku adalah aplikasi web yang bertujuan untuk mengedukasi mengenai peraturan zakat yang harus dikeluarkan dan melakukan perhitungan zakat sesuai aturan yang telah ditentukan menurut Islam. Aplikasi web ini dibuat berdasarkan masalah di masyarakat yang belum mengetahui tentang penghitungan zakat. Dengan adanya website ini diharapkan dapat memberikan solusi atas permasalahan tersebut dengan memberikan fitur-fitur sekaligus edukasi beragam zakat yang ada pada Islam kepada masyarakat dengan mudah digunakan dan dimengerti oleh masyarakat.</p>
+        </div>
+      </section>
+      <section id="content" class="content">
+        <div class="first-content">
+          <div class="img-first-content">
+            <img src="logo-content1.png" alt="image-first-content">
+          </div>
+          <div class="description-firts-content">
+            <h2>APA ITU ZAKAT?</h2>
+            <p>Zakat dalam segi istilah adalah harta tertentu yang wajib dikeluarkan oleh orang yang beragama Islam dan diberikan kepada golongan yang berhak menerimanya (fakir miskin dan sebagainya). Zakat dari segi bahasa berarti 'bersih', 'suci', 'subur', 'berkat' dan 'berkembang'. Menurut ketentuan yang telah ditetapkan oleh syariat Islam. Zakat merupakan rukun ketiga dari rukun Islam.</p>
+          </div>
+        </div>
+        <div class="second-content">
+        <div class="description-second-content">
+          <h2>KENAPA KITA HARUS ZAKAT ?</h2>
+          <p>Zakat merupakan salah satu kewajiban yang sudah seharusnya kita tunaikan. Sebagaimana Allah Ta’ala berfirman : “Wahai orang-orang yang beriman, infakkanlah (zakat) sebagian dari hasil usahamu yang baik-baik.” (QS. Al-Baqarah: 267). Zakat merupakan salah satu syari’at Allah yang sudah ada pada zaman dahulu. Orang - orang yang berzakat Allah akan memberikan ketentraman didalam hidupnya dan menjauhkan dari sifat kikir dan bakhil.</p>
+        </div>
+          <div class="img-second-content">
+            <img src="logo-content2.png" alt="image-second-content">
           </div>
         </div>
       </section>
