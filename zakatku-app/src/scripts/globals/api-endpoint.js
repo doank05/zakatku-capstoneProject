4bff53c972e3f6986f6b100ffbbfb12b321aa597
doank05 <dickysyahfitralbs@gmail.com>
2021-12-23 22:29:43 +0700

    Pembuatan file untuk konsumsi API

---
+++ zakatku-app/src/scripts/globals/api-endpoint.js
@@ -0,0 +1,7 @@
+/* eslint-disable linebreak-style */
+import CONFIG from './config';
+
+const API_ENDPOINT = {
+  TOP_HEADLINE: `${CONFIG.BASE_URL}/berita`,
+};
+export default API_ENDPOINT;
---
+++ zakatku-app/src/scripts/globals/config.js
@@ -0,0 +1,6 @@
+/* eslint-disable linebreak-style */
+const CONFIG = {
+  BASE_URL: 'https://61a5eead8395690017be8f38.mockapi.io',
+};
+
+export default CONFIG;
